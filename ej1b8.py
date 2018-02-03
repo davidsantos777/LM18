927e065eca9a9248f127c3f807f7381a3bcc552e
David Santos <seven_dvd@hotmail.com>
2018-02-03 19:41:21 +0100

    Añadido ejercicio 1 del boletín 8

---
+++ ej1b8.py
@@ -0,0 +1,9 @@
+from funciones import convertir_a_euros, pasar_a_segundos, calcular_coste
+
+acumulador = 0
+
+costes_de_tarifa = []
+
+tarifa = input("Escribe la tarifa por segundos en céntimos: ")
+
+comunicaciones = int(input("Escribe el número de comunicaciones realizadas: "))
