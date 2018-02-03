0dcf0b34df2c3f3fc64c24a028df5551fa560dd0
David Santos <seven_dvd@hotmail.com>
2018-02-03 19:36:22 +0100

    Agregado funciones.py sin errores

--- funciones.py
+++ funciones.py
@@ -1,13 +1,15 @@
 def convertir_a_euros(coste):
 		euros = int(str(coste/100).split('.')[0])
 		centi = int(str(coste/100).split('.')[1])
+		return euros, centi
 
-def calcular_coste(s, tarifa):
+def calcular_coste(segundos, tarifa):
+	return segundos * int(tarifa)
 
-		return s * int(tarifa)
-
-def pasar_a_segundos(h, m, s):
-		if h > 24 or m > 59 or s > 59:
+def pasar_a_segundos(horas, minutos, segundos):
+		if horas > 24 or minutos > 59 or segundos > 59:
 				return 0
 		else:
-				return (h * 3600) + (m * 60) + s						
+				return (horas * 3600) + (minutos * 60) + segundos
+
+						
