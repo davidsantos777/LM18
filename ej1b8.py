8066545c5022910cb995c7993920748fe45e1530
David Santos <seven_dvd@hotmail.com>
2018-02-03 20:06:14 +0100

    Añado al boletin split de las horas, minutos y segundos

--- ej1b8.py
+++ ej1b8.py
@@ -7,3 +7,13 @@ costes_de_tarifa = []
 tarifa = input("Escribe la tarifa por segundos en céntimos: ")
 
 comunicaciones = int(input("Escribe el número de comunicaciones realizadas: "))
+
+for i in range(comunicaciones):
+
+	tiempo = input("¿Cuánto tiempo duró la comunicación {}? (HH:MM:SS): ".format(i + 1))
+
+	horas = int(tiempo.split(':')[0])
+
+	minutos = int(tiempo.split(':')[1])
+
+	segundos = int(tiempo.split(':')[2])
