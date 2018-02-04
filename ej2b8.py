9ebcba480d3206834ce91b06d7f4830115fc2767
David Santos <seven_dvd@hotmail.com>
2018-02-04 19:36:16 +0100

    añado split de horas minutos y segundos

--- ej2b8.py
+++ ej2b8.py
@@ -9,3 +9,29 @@ acumulador = 0
 lista = []
 
 costes_de_tarifa = []
+
+for i in lines:
+
+	lista.append(i.strip('\n'))
+
+tarifa = lista[0].lstrip("Tarifa ")
+
+lista.pop(0)
+
+for i in lista:
+
+	horas = int(i.split(':')[0])
+
+	minutos = int(i.split(':')[1])
+
+	segundos = int(i.split(':')[2])
+
+	coste = calcular_coste(pasar_a_segundos(horas,minutos,segundos),tarifa)
+
+	acumulador += coste
+
+	euros, centi = convertir_a_euros(coste)
+
+	done = (euros,centi)
+
+	costes_de_tarifa.append(done)
